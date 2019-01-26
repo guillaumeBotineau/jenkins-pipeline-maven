babaeebf529d3e144ec019520df9763d2b46f610
Thomah <5090230+Thomah@users.noreply.github.com>
2019-01-26 17:50:59 +0100

    Add default Spring Boot project

---
+++ src/main/java/fr/valeuriad/learn/jenkinspipelinemaven/JenkinsPipelineMavenApplication.java
@@ -0,0 +1,14 @@
+package fr.valeuriad.learn.jenkinspipelinemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineMavenApplication.class, args);
+	}
+
+}
+
